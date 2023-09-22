a9d4c1fec15bc3e022fe34ca2c3c281e22e82549
Yang Heejenog <yanghj990@gmeil.com>
2023-09-22 13:41:19 +0900

    2023.09.22 5교시

--- board-back/src/main/java/com/heej/boardback/controller/MainController.java
+++ board-back/src/main/java/com/heej/boardback/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.heej.boardback.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     @GetMapping("")
-    public String serverCheck() {
-        return "Server on...";
+    public ResponseEntity<String> serverCheck() {
+        return ResponseEntity.status(HttpStatus.OK).body("Server On...");
     }
     
 }
--- board-back/src/main/java/com/heej/boardback/repository/UserRepository.java
+++ board-back/src/main/java/com/heej/boardback/repository/UserRepository.java
@@ -8,5 +8,9 @@ import com.heej.boardback.entity.UserEntity;
 @Repository
 // <엔터티, Id 타입>
 public interface UserRepository extends JpaRepository<UserEntity, String> {
+
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+    boolean existsByTelNumber(String telNumber);
     
 }
