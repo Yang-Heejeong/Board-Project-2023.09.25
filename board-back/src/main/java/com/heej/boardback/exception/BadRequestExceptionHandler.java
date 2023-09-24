fda8e85aff2461a2b8d8b6676d2ff4cc443b3c02
Yang Heejenog <yanghj990@gmeil.com>
2023-09-25 08:40:44 +0900

    first commit

--- board-back/src/main/java/com/heej/boardback/exception/BadRequestExceptionHandler.java
+++ board-back/src/main/java/com/heej/boardback/exception/BadRequestExceptionHandler.java
@@ -14,6 +14,6 @@ public class BadRequestExceptionHandler {
     @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
     public ResponseEntity<ResponseDto> validationExceptionHandler(Exception exception){
         return ResponseDto.validationFailed();
-    }
+    } 
 
 }
