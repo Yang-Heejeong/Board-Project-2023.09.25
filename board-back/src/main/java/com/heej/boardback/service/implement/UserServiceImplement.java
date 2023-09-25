f09e200ae0108b7a767ffccdbfa13a02f5de3afb
Yang Heejenog <yanghj990@gmeil.com>
2023-09-25 12:53:40 +0900

    2023.09.25 4교시

---
+++ board-back/src/main/java/com/heej/boardback/service/implement/UserServiceImplement.java
@@ -0,0 +1,14 @@
+package com.heej.boardback.service.implement;
+
+import org.springframework.stereotype.Service;
+
+import com.heej.boardback.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImplement implements UserService {
+    
+}
