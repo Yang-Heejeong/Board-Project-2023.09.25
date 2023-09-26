c786a7e7481ff1820f795c60acb70bfb01d21783
Yang Heejenog <yanghj990@gmeil.com>
2023-09-26 10:56:05 +0900

    2023.09.26 2교시

---
+++ board-back/src/main/java/com/heej/boardback/service/FileService.java
@@ -0,0 +1,9 @@
+package com.heej.boardback.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileService {
+    
+    String upload(MultipartFile file);
+
+}
---
+++ board-back/src/main/java/com/heej/boardback/service/implement/FileServiceImplement.java
@@ -0,0 +1,17 @@
+package com.heej.boardback.service.implement;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.heej.boardback.service.FileService;
+
+@Service
+public class FileServiceImplement implements FileService {
+
+    @Override
+    public String upload(MultipartFile file) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'upload'");
+    }
+    
+}
